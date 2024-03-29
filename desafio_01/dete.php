9c8874acebc7a41acb731a2c2e5db8f709c1357b
lauriciodev <developerwebmasterpro@gmail.com>
2024-03-29 14:01:27 -0300

    date

---
+++ desafio_01/dete.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Date</title>
+</head>
+<body>
+  <?php 
+ // definindo  retorno da data em portugues;
+  setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+
+  date_default_timezone_set('America/Sao_Paulo');
+
+  echo strftime('%A, %d de %B de %Y', strtotime('today'));
+  echo "<br>";
+   $dataHoras = date("h:i:s a");
+   // h horas no formato 12 horas 
+
+   $dataDias = date("D:M:Y");
+   // os parametros em maiusculo exibe o nome por completo
+
+   echo $dataHoras;
+   echo "<br>";
+   echo $dataDias;
+  ?>
+  
+</body>
+</html>
