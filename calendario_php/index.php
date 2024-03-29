c17d2814c9c4fa9427b5147f25fff0f892ba395f
lauriciodev <developerwebmasterpro@gmail.com>
2024-03-29 14:01:13 -0300

    date

--- calendario_php/index.php
+++ calendario_php/index.php
@@ -33,17 +33,17 @@
 
       <?php 
       function Linha($semana){
-        echo "
-        <tr>
-         <td>{$semana[0]}</td>
-         <td>{$semana[1]}</td>
-         <td>{$semana[2]}</td>
-         <td>{$semana[3]}</td>
-         <td>{$semana[4]}</td>
-         <td>{$semana[5]}</td>
-         <td>{$semana[6]}</td>
-        </tr>
-        ";
+    
+      echo "<tr>";
+    for($i = 0; $i <= 6; $i++){
+      if(isset($semana[$i])){
+        echo "<td>{$semana[$i]}</td>";
+      }else{
+        echo "<td></td>";
+      }
+    }
+        echo "</tr>";
+      
       }
 
       function Calendario(){
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+   echo "lauricio de sousa";
+
+?>
