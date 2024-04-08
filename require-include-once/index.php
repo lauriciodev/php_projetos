f0ead4af1cfb78e9c5d5a25c858e539cc5124602
lauriciodev <developerwebmasterpro@gmail.com>
2024-04-08 15:29:54 -0300

    require-once

---
+++ require-include-once/index.php
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+<?php 
+//require_once e include_once servem para que um arquivo nao seja incluido mais de uma vez
+
+
+
+
+include_once "teste1.php";
+include_once "teste1.php";
+
+// o mesmo vale para o require_once
+?>
