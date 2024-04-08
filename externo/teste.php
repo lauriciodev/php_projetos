bac179ebce30055cbf28720f2dc62f7a341c2c32
lauriciodev <developerwebmasterpro@gmail.com>
2024-04-08 14:32:44 -0300

    dir

---
+++ externo/teste.php
@@ -0,0 +1,1 @@
+<h4>Sou um arquivo externo</h4>
--- require/index.php
+++ require/index.php
@@ -1,5 +1,7 @@
 <?php 
-require "teste2.php";
+require __DIR__ . "/../externo/teste.php";
+
+//acima esta uma forma de adicionar arquivos externosc
 
 //no caso do require o codigo ira parar de funcionar caso o arquivo importado nao exista;
 ?>
