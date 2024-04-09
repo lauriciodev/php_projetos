dcfd9b84e5726ac0cc9b6e01b0dfde2f86dae9fc
lauriciodev <developerwebmasterpro@gmail.com>
2024-04-09 15:41:30 -0300

    short-tag

---
+++ short-tag/index.php
@@ -0,0 +1,13 @@
+<?php 
+// short tags é uma forma alternativa de inserir codigos php
+// deve ser habilitado esta configuração no servidor php  apache =>php.ini
+
+
+echo "<h4>codigo normal</h4>";
+
+?>
+
+
+<?
+echo "codigo short tag";
+?>
