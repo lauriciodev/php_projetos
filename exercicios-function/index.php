a475774a25e467f7815c857b505dae44e429e30d
lauriciodev <developerwebmasterpro@gmail.com>
2024-04-14 10:11:00 -0300

    function

---
+++ exercicios-function/index.php
@@ -0,0 +1,21 @@
+<?php
+// crie um array com strings
+// ultilize a função implode no array
+// primeiro argumento ","
+// segundo argumento o seu array
+// atribua o retorno da função  a uma variavel
+// exiba o resultado 
+
+
+ $myArray = ["lau","shark","white","arc", "2708"];
+
+$myRetunr = implode(" ", $myArray);
+// função implode retorna uma  string com todos os item do array junto 
+// seprados pelo argumento passado na função
+
+
+
+echo $myRetunr;
+
+
+?>
